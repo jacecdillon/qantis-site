a085ebc0b9e22863499e2996139123d410a2d6bb
s00ndie <178602384+s00ndie@users.noreply.github.com>
2026-09-02 13:52:02 +0200

    meer velden

--- wp-content/themes/qantis/page-propositie.php
+++ wp-content/themes/qantis/page-propositie.php
@@ -6,15 +6,12 @@ $primaire_button = get_field('primaire_button');
 $secondary_button = get_field('secondary_button');
 $accentkleur = get_field('accentkleur');
 $diensten = get_field('diensten');
+$stap = get_field('stappen');
 echo '<pre>';
 print_r($primaire_button);
 echo '</pre>';
 
-echo '<pre>';
-print_r($diensten);
-echo '</pre>';
 ?>
-
 <main>
     <span>
         <?php echo get_field('hero_tag'); ?>
@@ -40,7 +37,30 @@ echo '</pre>';
     <p>
         Accentkleur: <?php echo $accentkleur; ?>
     </p>
+   <?php foreach($diensten as $dienst) {?>
+    <h2>
+        <img src="<?php echo $dienst['icoon']['url']; ?>" alt="">
+        <?php echo $dienst['titel']?>
+        <?php echo $dienst['tekst']?>
 
+    </h2>
+    <?php } ?>
 
+    <p>
+        <?php echo get_field('section_tag'); ?>
+    </p>
+    <h1>
+        <?php echo get_field('titel'); ?>
+    </h1>
+    <p>
+        <?php echo get_field('subtekst'); ?>
+    </p>
+    <img src="<?php echo get_field('afbeelding')?>" alt="">
+    <?php foreach($stap as $stappen) {?>
+    <h2><?php echo $stappen['titel']; ?></h2>
+    <p><?php echo $stappen['tekst']; ?></p>
+    <?php } ?>
+
+    
 </main>
 <?php get_footer()?>
